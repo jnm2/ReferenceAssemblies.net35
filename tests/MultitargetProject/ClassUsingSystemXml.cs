17d107f6e048e9940e25f5653c6270dd81b7a9c8
jnm2 <me@jnm2.com>
2019-11-24 22:57:55 -0500

    Add tests

---
+++ tests/MultitargetProject/ClassUsingSystemXml.cs
@@ -0,0 +1,9 @@
+using System.Xml;
+
+namespace Net35Project
+{
+    public class ClassUsingSystemXml
+    {
+        public XmlDocument Document { get; set; }
+    }
+}
---
+++ tests/Net35Project/ClassUsingSystemLinq.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+namespace Net35Project
+{
+    public class ClassUsingSystemLinq
+    {
+        public ClassUsingSystemLinq()
+        {
+            Enumerable.Empty<int>();
+        }
+    }
+}
